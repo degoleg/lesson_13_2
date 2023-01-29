8a4142e25793316a2d48e4e62bdbfc735bf4fd73
Олега <degoleg@yandex.ru>
2023-01-29 21:36:45 +0400

    import DAO

--- candidates_dao.py
+++ candidates_dao.py
@@ -1,11 +1,23 @@
 import json
 
+from candidate import Candidate
+
 
 class CandidatesDAO:
 
     def load_data(self):
+
         with open("candidates.json", "r", encoding="utf-8") as file:
-            candidates = json.load(file)
+            candidates_data = json.load(file)
+            candidates = [] # Создаем список объектов класса Candidate
+
+            for candidate in candidates_data:
+                candidates.append(Candidate(
+                    candidate["id"],
+                    candidate["name"],
+                    candidate["position"],
+                    candidate["skills"]
+                ))
         return candidates
 
     def get_all(self):
@@ -17,14 +29,15 @@ class CandidatesDAO:
         skill_lower = skill.lower()
 
         for candidate in candidates:
-            candidate_skills = candidate["skills"].lower().split(", ")
+            candidate_skills = candidate.skills.lower().split(", ")
             if skill_lower in candidate_skills:
                 skilled_candidates.append(candidate)
 
         return skilled_candidates
 
+
     def get_by_id(self, candidate_id):
         candidates = self.load_data()
         for candidate in candidates:
-            if candidate["id"] == candidate_id:
+            if candidate.id == candidate_id:
                 return candidate
